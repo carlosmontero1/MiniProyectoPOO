e65a012a804321e521c44ff262f0c0ccebc7875e
carlosmontero1 <93013024+carlosmontero1@users.noreply.github.com>
2022-06-09 01:43:50 -0500

    Add files via upload

---
+++ MiniProyectoPoo/src/ec/edu/utpl/poo/semana8/proyecto/Main.java
@@ -0,0 +1,30 @@
+package ec.edu.utpl.poo.semana8.proyecto;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // crear un objeto de la clase Motorcycle enviando elementos para inicializar los valores
+        Motorcycle motorcycle1 = new Motorcycle("BMW", "Black", 2020);
+
+        // mostrar datos utilizando metodos get para retornar valores
+        System.out.println("Brand: " + motorcycle1.getBrand());
+        System.out.println("Color: " + motorcycle1.getColor());
+        System.out.println("Year: " + motorcycle1.getYear());
+
+        // cambiamos el valor del año con el metodo set a un numero que no este incluido en el rango
+
+        System.out.println("\nYear change");
+
+        // utilizamos el metodo get para mostrar el año ingresado anterior
+
+        System.out.println("Previous Year: \t" + motorcycle1.getYear());
+
+        // utilizamos el metodo set para otorgar un nuevo valor a year
+        motorcycle1.setYear(2010);
+
+        // mostrar el valor modificado del objeto
+        System.out.println("New Year: \t" + motorcycle1.getYear());
+
+    }
+}
---
+++ MiniProyectoPoo/src/ec/edu/utpl/poo/semana8/proyecto/Motorcycle.java
@@ -0,0 +1,63 @@
+package ec.edu.utpl.poo.semana8.proyecto;
+import java.time.LocalDate;
+public class Motorcycle {
+
+    // atributos
+
+    private String brand;
+    private String color;
+    private int year;
+
+    // constructor
+
+    public Motorcycle(String brand, String color, int year) {
+        this.brand = brand;
+        this.color = color;
+        if (year > 1885 && year < LocalDate.now().getYear()) {
+            this.year = year;
+        } else {
+            throw new IllegalArgumentException("El año de la motocycleta no puede ser antes de 1885 o mayor a este año");
+        }
+    }
+
+    // sobrecarga de constructores
+
+    public Motorcycle(String brand) {
+        this.brand = brand;
+    }
+
+    public Motorcycle(String brand, String color) {
+        this.brand = brand;
+        this.color = color;
+    }
+
+    // metodos
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        if (year > 1885 && year < LocalDate.now().getYear()) {
+            this.year = year;
+        } else {
+            throw new IllegalArgumentException("El año de la motocycleta no puede ser antes de 1885 o mayor a este año");
+        }
+    }
+}
